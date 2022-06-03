4942ddb7ab70f1bc139f56ad46bb74fb984877ee
Choijinyoung-9878 <chlwlsdud69@naver.com>
2022-06-03 10:32:00 +0900

    06-03

--- source/06_JSP/ch13_board/WebContent/db/board.sql
+++ source/06_JSP/ch13_board/WebContent/db/board.sql
@@ -17,3 +17,44 @@ CREATE TABLE BOARD(
 );
 
 SELECT * FROM BOARD;
+
+-- 1. 글 개수
+SELECT COUNT(*) FROM BOARD;
+
+-- 2. 글 목록 (최신글이 위로 )
+SELECT * FROM BOARD ORDER BY NUM DESC;
+
+-- 3. 글쓰기 (원글쓰기)
+    -- NUM 글번호 받아오는 서브쿼리 가장 큰값에서 +1 
+        SELECT NVL(MAX(NUM) , 0) +1 FROM BOARD;
+        
+INSERT INTO BOARD (NUM , WRITER , SUBJECT , CONTENT, EMAIL, PW, REF, RE_STEP, RE_INDENT, IP )
+    VALUES ( (SELECT NVL(MAX(NUM) , 0) +1 FROM BOARD) , '홍길동' , '글제목1' , '본문입니다\n금요일방가' , NULL, '1' , (SELECT NVL(MAX(NUM) , 0) +1 FROM BOARD) , 0 , 0 , '192.168.10.30'  );
+INSERT INTO BOARD (NUM , WRITER , SUBJECT , CONTENT, EMAIL, PW, REF, RE_STEP, RE_INDENT, IP )
+    VALUES ( (SELECT NVL(MAX(NUM) , 0) +1 FROM BOARD) , '오동준' , '글제목2' , '본문입니다\n토요일열심히공부하자' , NULL, '1' , (SELECT NVL(MAX(NUM) , 0) +1 FROM BOARD) , 0 , 0 , '192.168.10.30'  );
+INSERT INTO BOARD (NUM , WRITER , SUBJECT , CONTENT, EMAIL, PW, REF, RE_STEP, RE_INDENT, IP )
+    VALUES ( (SELECT NVL(MAX(NUM) , 0) +1 FROM BOARD) , '대환콩' , '글제목3' , '본문입니다\n나이스금요일' , NULL, '1' , (SELECT NVL(MAX(NUM) , 0) +1 FROM BOARD) , 0 , 0 , '192.168.10.30'  );    
+    
+-- 4. 글번호로 글 (DTO) 가져오기
+SELECT * FROM BOARD WHERE NUM=1;
+
+-- 5.조회수 올리기
+UPDATE BOARD SET READCOUNT = READCOUNT+1 WHERE NUM=1;
+
+-- 6. 글 수정 
+UPDATE BOARD SET SUBJECT = '수정된 제목1',
+                                        CONTENT = '수정된 본문입니다\n와우',
+                                            EMAIL = 'hong@hong.com',
+                                                PW='1',
+                                                    IP='127.0.0.1'
+                                            WHERE NUM=1;
+
+-- 7. 글 삭제(비밀번호)
+DELETE FROM BOARD WHERE NUM=1 AND PW='1';
+
+-- 조회수 조작
+UPDATE BOARD SET READCOUNT = 12 WHERE NUM=3;
+
+COMMIT;
+
+
